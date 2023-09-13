5ddd697ce875c3a5b6ac8a2a79cad3a0b8d69638
Julio César Alves <julio.caburu@gmail.com>
2023-09-12 22:21:41 -0300

    Versão inicial

---
+++ Livro.java
@@ -0,0 +1,26 @@
+/**
+ * Uma classe que mantém informação de um livro.
+ * Ela pode ser parte de uma aplicação mair como um sistema
+ * de uma biblioteca, por exemplo.
+ *
+ * @author (Digite seu nome aqui.)
+ * @version (Insira o dia de hoje aqui.)
+ */
+class Livro
+{
+    // Os atributos.
+    private String autor;
+    private String titulo;
+
+    /**
+     * Define os atributos autor e o título quando este
+     * objeto é criado.
+     */
+    public Livro(String autorLivro, String tituloLivro)
+    {
+        autor = autorLivro;
+        titulo = tituloLivro;
+    }
+
+    // Adicione os métodos aqui ...
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Projeto `Exercicio-Livro`
+
+Autores: David Barnes and Michael Kölling
+
+- Traduzido por: Julio César Alves
+
+Este projeto é parte do material do livro
+
+```
+   Objects First with Java - A Practical Introduction using BlueJ
+   Sixth edition
+   David J. Barnes and Michael Kölling
+   Pearson Education, 2016
+```
+
+É discutido no capítulo 2.
+
+## Objetivo do projeto
+
+Este projeto tem o código inicial para um conjunto de exercícios do Capítulo 2.
+
+## Instruções para o usuário
+
+Complete os exercícios do Capítulo 2.
